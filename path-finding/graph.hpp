1c7fd8d464cebf21bb42082f1d08000c09c5d698
Satyam <patelsatyam100@gmail.com>
2023-01-13 22:27:01 -0500

    added graph traversal

--- path-finding/graph.hpp
+++ path-finding/graph.hpp
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <iostream>
 #include <queue>
 #include <map>
+#include <deque>
 
 
 /*
@@ -17,9 +19,20 @@ connected
 
 */
 
-class Graph {
+/*
+Color coding for each node!
 
+0: White: Undiscovered
+
+1: Gray: Currently exploring
+
+2: Black: Finished and fully discovered!
+*/
+
+class Graph {
     public:
+        int vnum; 
+
         struct Node {
             int value;
             int color; 
@@ -30,7 +43,8 @@ class Graph {
         std::vector<Node> vertexSet; 
 
         std::map<std::pair<int, int>, int> edgeSet; 
-        Graph(int nodesize) {
+
+        Graph(int nodesize) : vnum(nodesize) {
             vertexSet.reserve(sizeof(Node) * nodesize); 
             for (int i = 0; i < nodesize; i++) 
                 vertexSet.push_back(Node(i));
@@ -38,13 +52,99 @@ class Graph {
         }
 
 
-        std::pair<int, int> createEdge(int node1, int node2, int weight) {
+        void createEdge(int node1, int node2, int weight) {
             edgeSet.insert(std::pair<std::pair<int, int>, int>(std::make_pair(node1, node2), weight)); 
-            return std::make_pair(node1, node2);
+            // return std::make_pair(node1, node2);
         }
 
 
-    private:
+        std::vector<int> bellmanFord() {
+            vertexSet[0].value = 0;
+            vertexSet[0].color = 2;
+            std::deque<Node> vals;
+            vals.push_back(vertexSet[0]);
+            
+            for (int i = 1; i < this->vnum; i++) {
+                this->vertexSet[i].value = 10000; 
+                this->vertexSet[i].color = 0;
+            }
+
+            int count = 0;
+
+
+            while (vals.size() != 0) {
+                for (auto it = edgeSet.begin(); it != edgeSet.end(); it++) {
+                    if (vals.front().index == it->first.first) {
+                        checkWeight(it, vertexSet[it->first.first], vertexSet[it->first.second]);
+                        if (vertexSet[it->first.second].color != 3)
+                            vals.push_back(vertexSet[it->first.second]);
+                    }
+
+                    std::cout << "vals.front() : " << vals.front().index << "\n" << std::endl; 
+                }
+                vertexSet[vals.front().index].color = 3;
+                vals.pop_front();
+            }
+            return {0, 2, 3};
+        }
+
+        bool checkWeight(std::map<std::pair<int,int>, int>::iterator it, Node &node1, Node &node2) {
+            printf("\nNode 1: %d\nNode 2: %d\nWeight: %d\nNode %d val: %d\nNode %d val: %d\n\n\n\n", it->first.first, it->first.second, it->second, node1.index, node1.value, node2.index, node2.value);
+            
+            // if parent node is undiscovered, return false. 
+            if (node1.color == 0) 
+                return false;
+            else    
+                node2.color = 2; // set child node to gray. 
+
+            
+                
+            // if child node value is less than parent node + weight, then false. 
+            if (node2.value > (node1.value + it->second)) {
+                node2.value = node1.value + it->second;
+                std::cout << node2.index << ":" << node2.value << std::endl;
+                return true;
+            }
+
+            return false;
+
+        }
+
+
+        void printEdgeSet() {
+            for (auto it = this->edgeSet.begin(); it != this->edgeSet.end(); it++) 
+                printf("\n\nNode 1: %d\n\n Node 2: %d\n\n Weight: %d\n\n", it->first.first, it->first.second, it->second); 
+        }
+
+
+
+
+
+
+
+
+
+        void BFS() {
+            std::queue<int> grayPeople;
+            std::vector<int> blackPeople; 
+
+            grayPeople.push(0);
+
+            int holder = 0; 
+ 
+            while (grayPeople.size() != 0) {
+                holder = grayPeople.front();
+                printf("\nNode %d has finally achieved Black Person Status (BPS)\n\n", holder);
+                for (auto it = this->edgeSet.begin(); it != this->edgeSet.end(); it++) {
+                    if (it->first.first == holder) {
+                        grayPeople.push(it->first.second); 
+                        printf("\nPushing node %d onto Gray People Queue (GPQ)\n\n", it->first.second);
+                    }
+                }
+                grayPeople.pop();
+            }
+
+        }
 
 
 };
--- path-finding/main.cpp
+++ path-finding/main.cpp
@@ -2,20 +2,49 @@
 #include "graph.hpp"
 #include <vector>
 #include <queue>
+#include <string.h>
 
-
-const int NODE = 2;
+bool tryme = 1;
+const int NODE = 10;
 
 int main() {
-    Graph newGraph = Graph(5);
+    Graph newGraph = Graph(NODE);
     std::vector<int> vertexSet;
+    int p_node;
+    int c_node;
+    int weight;
+    char cont; 
 
     for (int i = 0; i < NODE; i++)
         vertexSet.push_back(newGraph.vertexSet[i].index);
 
-    std::pair<int, int> edge = newGraph.createEdge(newGraph.vertexSet[0].index, newGraph.vertexSet[1].index, 5);
+    while (tryme) {
+        printf("\nEnter a parent node: ");
+        std::cin >> p_node;
+        fflush(stdin);
+        printf("\nEnter a child node: ");
+        std::cin >> c_node; 
+        fflush(stdin);
+        printf("\nEnter a weight: ");
+        std::cin >> weight; 
+        fflush(stdin);
+    
+        newGraph.createEdge(p_node, c_node, weight);
 
-    printf("\nEdge stored is: %d\n", newGraph.edgeSet[std::make_pair(edge.first, edge.second)]);
+        printf("\nEnter another edge? [Y/n]\n");
+        std::cin >> cont; 
+        fflush(stdin);
+        printf("\n\n%c\n\n", cont);
+        if ('Y' != cont)
+            tryme = 0; 
+    }
+
+    newGraph.printEdgeSet();
+
+
+    newGraph.bellmanFord();
+
+
 
 
     /*
